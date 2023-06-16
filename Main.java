4a2059769f12026ad2ef5fb10c6ac67f04429451
Jeongsu6637 <wjdtn8738@gmail.com>
2023-06-16 09:58:25 +0900

    test

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
+import config.JdbcConnection;
+
+import java.sql.Connection;
+
 public class Main {
     public static void main(String[] args) {
-
+//        여기가 메인
+        Connection conn = new JdbcConnection().getJdbc();
     }
 }
